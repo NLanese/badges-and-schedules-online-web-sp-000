92297e8e9fdb83ccb5c82735d4d4dfcbde4dffba
Nick Lanese <nlanese21@gmail.com>
2020-05-28 22:48:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  return_array = []
+  array.each do | name |
+    return_array << badge_maker(name)
+  end
+  return return_array
+end
+
+def assign_rooms(name_array)
+  return_array = []
+  room = 1
+  name_array.each do | name |
+    return_array << "Hello, #{name}! You'll be assigned to room #{room}!"
+    room = room + 1
+  end
+  return return_array
+end
+
+def printer(name_array)
+  puts batch_badge_creator(name_array)
+  puts assign_rooms(name_array)
+end
